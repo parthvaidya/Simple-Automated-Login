678359351f8640ec05f4b8a480fd58be6f4bf50b
Sagar Gupta <Sroyalgupta@gmail.com>
2020-09-30 21:16:08 +0530

    Added error handling methods

--- automatelogin.py
+++ automatelogin.py
@@ -7,12 +7,18 @@ password = getpass("Enter your password")
 driver = webdriver.Chrome("D:\\chromedriver_win32\\chromedriver.exe")
 driver.get("https://www.facebook.com/")
 
-username_textbox = driver.find_element_by_id("email")
-username_textbox.send_keys(username) 
+try:
+  username_textbox = driver.find_element_by_id("email")
+  username_textbox.send_keys(username) 
+except NoSuchElementException:
+  print('Element not found')
 
+try:
+  password_textbox = driver.find_element_by_id("pass")
+  password_textbox.send_keys(password)
+except NoSuchElementException:
+  print('Element not found')
 
-password_textbox = driver.find_element_by_id("pass")
-password_textbox.send_keys(password)
-
+  
 login_button = driver.find_element_by_id("loginbutton")
 login_button.submit()
